58d272db0095f560588132bfc2855e678d8064b0
andrewteece <andrewteece@gmail.com>
2024-08-13 08:54:19 -0500

    cleaup files not used

--- Github Finder/src/App.jsx
+++ Github Finder/src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className='text-3xl font-bold underline mb-11'>Hello World!</h1>
-      <button className='btn btn-primary'>Daisy buttonb</button>
     </>
   );
 }

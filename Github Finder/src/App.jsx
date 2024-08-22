fe43e72a08535da9b8840dcafafb33a23e619d50
andrewteece <andrewteece@gmail.com>
2024-08-22 17:31:17 -0500

    update App.jsx and create User.jsx

--- Github Finder/src/App.jsx
+++ Github Finder/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
+import Home from './pages/Home'
+import About from './pages/About'
+import User from './pages/User'
+import NotFound from './pages/NotFound'
+
 import { GithubProvider } from './context/github/GithubContext'
 import { AlertProvider } from './context/alert/AlertContext'
 
@@ -15,11 +18,19 @@ function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar />
 
-            <main className='container mx-auto px-3'>
+            <main className='container mx-auto px-3 pb-12'>
               <Routes>
-                <Route path='/' element={<Home />} />
-
-                <Route path='/About' element={<About />} />
+                <Route
+                  path='/'
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='*' element={<NotFound />} />
               </Routes>
